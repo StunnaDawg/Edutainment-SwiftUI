267dcf961df8bf0d31e1b6f7a051b7bd14362f88
StunnaDawg <jonsonallen9@gmail.com>
2023-08-23 10:19:32 -0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Jonson Allen on 2023-08-23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
